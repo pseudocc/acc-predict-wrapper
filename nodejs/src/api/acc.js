d579ace489a5af9801be0ec923082098dbbe4a9c
pseudocc <pseudoc@163.com>
2021-12-14 20:31:35 +0800

    bug fix

--- nodejs/src/api/acc.js
+++ nodejs/src/api/acc.js
@@ -37,7 +37,7 @@ class AccApi {
    */
   queryVersion() {
     const url = `${this.host}/api/texttospeech/v3.0-beta1/voicegeneraltask/versions`;
-    const p = axios.get(url, null, this.axiosConfig);
+    const p = axios.get(url, this.axiosConfig);
     return p.then(resp => {
       if (resp.status == 200)
         return resp.data;
