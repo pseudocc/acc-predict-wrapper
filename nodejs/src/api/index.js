4a6174b5d90e5dc711547f7b15b54eebe522008f
pseudocc <pseudoc@163.com>
2021-12-14 17:01:14 +0800

    add localhost testing

--- nodejs/src/api/index.js
+++ nodejs/src/api/index.js
@@ -5,11 +5,15 @@ const AccApi = require('./acc');
 /**
  * Base URL for all REST API calls
  * @param {string} region where your cognitive service - text to speech creates
+ * @param {number} port optional parameter indicating port for localhost server
  * @returns {HostURL|null} return null when region is not supported
  */
-function getHost(region) {
-  if (region)
+function getHost(region, port) {
+  if (region) {
+    if (region == 'localhost')
+      return `https://localhost:${port}`;
     return `https://${region.toLowerCase()}.customvoice.api.speech.microsoft.com`;
+  }
   return null;
 }
 
