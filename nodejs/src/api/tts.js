293fcf46bf56e6fc98a057124d5e02fd8808c84d
pseudocc (catlas) <pseudoc@163.com>
2022-05-12 16:05:31 +0800

    adjust offset returned from tts prediction api

--- nodejs/src/api/tts.js
+++ nodejs/src/api/tts.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const BaseApi = require('./base');
+const cheerio = require('cheerio');
 const { default: axios } = require('axios');
 
 class TtsApi extends BaseApi {
@@ -26,25 +27,71 @@ class TtsApi extends BaseApi {
   async predictRoleAndStyle(ssml) {
     if (ssml == null || ssml.length == 0)
       throw new Error('SSML is null or empty.');
+
     const match = /<voice\s+name="[^"]+"\s*>/.exec(ssml);
     const wrappedSsml = ssml.substring(0, match.index)
       + match[0]
       + '<mstts:task name="RoleStyle" />'
       + ssml.substring(match.index + match[0].length);
 
-    const resp = await axios.post(this.host, wrappedSsml, { headers: this.headers });
+    let resp;
+    let maxRetry = 10;
+    do {
+      try {
+        resp = await axios.post(this.host, wrappedSsml, { headers: this.headers });
+        break;
+      }
+      catch (e) {
+        const resp = e.response;
+        if (resp != null && (resp.status == 503 || resp.status == 429)) {
+          e.busy = 1;
+        }
+        if (maxRetry-- && delay) {
+          await sleep(1000);
+          continue;
+        }
+        throw e;
+      }
+    } while (maxRetry);
+
     if (resp.status != 200 || !resp.data.IsValid)
       return null;
 
     const seq = resp.data.Conversations.map(c => ({
       offset: c.Begin,
       length: c.End - c.Begin,
+      text: c.Content,
       role: c.Role,
       style: c.Style
     }));
 
-    return seq;
+    return adjust_offset(ssml, seq);
   }
 };
+
+/**
+ * offset returned by TTS API is not compatible with ACC API.
+ * @param {string} ssml 
+ * @param {import('./acc').ExpressAsObject[]} seq 
+ * @returns {import('./acc').ExpressAsObject[]}
+ */
+function adjust_offset(ssml, seq) {
+  const xml_obj = cheerio.load(ssml, { xml: true });
+  const inner_text = xml_obj.text();
+  let bias = 0;
+  for (const eo of seq) {
+    if (!eo.text)
+      throw new Error('Property text is null or empty and cannot be referred to: '
+        + `${JSON.stringify(eo)}.`);
+    const corrected_offset = inner_text.indexOf(eo.text, bias + eo.offset);
+    if (corrected_offset == -1)
+      throw new Error(`Failed to adjust offset for ${JSON.stringify(eo)}.`);
+    bias = corrected_offset - eo.offset;
+    eo.offset = corrected_offset;
+    delete eo.text;
+  }
+
+  return seq;
+}
 
 module.exports = TtsApi;
